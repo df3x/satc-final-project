fd0a7e755879aad016d700b340afe136582e4806
Dmitriy Fedorenko <dfxxx@inbox.ru>
2020-01-06 17:35:41 +0300

    Added product page test functional

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators:
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, "#messages .alert-info div p:nth-child(1) strong")
+    PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) div strong")
