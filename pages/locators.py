522aec0edb155a283741b31f0dfb137b834f807f
Dmitriy Fedorenko <dfxxx@inbox.ru>
2020-01-07 21:41:30 +0300

    go to login test replace to base page

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    pass
 
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -12,5 +15,6 @@ class ProductPageLocators:
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1)")
     PRODUCT_PRICE_IN_BASKET = (By.CSS_SELECTOR, "#messages .alert-info div p:nth-child(1) strong")
     PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) div strong")
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
+@pytest.mark.skip()
 @pytest.mark.parametrize('promo_offer', [pytest.param(i, marks=pytest.mark.xfail(i==7, reason='')) for i in range(10)])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
@@ -9,3 +11,47 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_product_added_to_basket()
+
+@pytest.mark.skip()
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#Открываем страницу товара
+#Добавляем товар в корзину
+#Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+#Открываем страницу товара
+#Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.skip()
+def test_message_disappeared_after_adding_product_to_basket(browser):
+#Открываем страницу товара
+#Добавляем товар в корзину
+#Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_appeared_success_message()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    LoginPage(browser, None).should_be_login_page()
